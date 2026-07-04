7c3c02e4f24f5edc9da68faa340064ad5532c757
Muhammad Ahmed Cheema <muhammad.cheema@sendoso.com>
2026-07-04 12:33:48 -0700

    Add the working rules and the live-test env loader

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,17 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
+# Live tests read provider keys from the gitignored env file; CI never has one.
+env_file = File.expand_path("../.env.development.local", __dir__)
+if File.exist?(env_file)
+  File.readlines(env_file).each do |line|
+    line = line.strip
+    next if line.empty? || line.start_with?("#")
+
+    key, value = line.split("=", 2)
+    ENV[key] ||= value.to_s.delete_prefix('"').delete_suffix('"')
+  end
+end
+
 require "mistri"
 require "minitest/autorun"
